58a90386c1ae276849e6ee2085ad1c5f0cca7643
taehwan-jang <jth0602@gmail.com>
2021-05-10 20:19:19 +0900

    :rocket: 2021.05.13

---
+++ src/utils/JSFunction.java
@@ -0,0 +1,30 @@
+package utils;
+
+import javax.servlet.jsp.JspWriter;
+
+public class JSFunction {
+	//JS를 통해 경고창을 띄우고 페이지를 이동한다.
+	public static void alertLocation(String msg, String url, JspWriter out) {
+		try {
+			String str = ""
+					+"<script>"
+					+" alert('"+msg+"'); "
+					+" location.href='"+url+"'; "
+					+"</script>";
+			out.println(str);
+		} catch (Exception e) {}
+	}
+	//경고창을 띄우고 뒤로 이동한다.
+	public static void alertBack(String msg, JspWriter out) {
+		try {
+			String str =""
+					+"<script>"
+					+" alert('"+msg+"'); "
+					+" history.back() "//history.go(-1)을 사용해도 된다.
+					+"</script>";
+			out.println(str);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
